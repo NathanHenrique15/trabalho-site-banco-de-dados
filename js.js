c40c7e8a7147c5c73bcfdafeaa38c28fb5706079
rickpene <166026646+rickpene@users.noreply.github.com>
2024-08-23 12:30:50 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+const nome=['natan', 'kaue', 'penenanete','zé1','zé2','zé3',]
+
+function carrega(){
+    for(var item in nome) {
+        if((item % 2)==0){
+            $('#esq').append('<div class="caixa"><h1>'+nome[item]+'</h1></div>');
+        }else{
+            $('#dir').append('<div class="caixa"><h1>'+nome[item]+' </h1></div>');
+        }
+        
+    }   
+   
+}
