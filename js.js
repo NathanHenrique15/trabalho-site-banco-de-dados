71704208a0e4041e6a99cb9a69a673417fc8b10c
NathanHenrique15 <165053258+NathanHenrique15@users.noreply.github.com>
2024-08-23 22:26:47 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
-const nome=['natan', 'kaue', 'penenanete','zé1','zé2','zé3',]
+const nome=['Inglês','Português','Quimica','Matemática','Geografia','Biologia',]
 
 function carrega(){
     for(var item in nome) {
         if((item % 2)==0){
-            $('#esq').append('<div class="caixa"><h1>'+nome[item]+'</h1></div>');
+            $('#esq').append('<div class="caixa"><a href="'+nome[item]+'.html" class=txtbox>'+nome[item]+'</a></div>');
         }else{
-            $('#dir').append('<div class="caixa"><h1>'+nome[item]+' </h1></div>');
+            $('#dir').append('<div class="caixa"><a href="'+nome[item]+'.html" class=txtbox>'+nome[item]+'</a></div>');
         }
         
     }   
