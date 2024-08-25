98c23479041fd6ca3b369c08ee854e30b89a6875
NathanHenrique15 <165053258+NathanHenrique15@users.noreply.github.com>
2024-08-24 23:08:09 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,13 +1,12 @@
 const nome=['Inglês','Português','Quimica','Matemática','Geografia','Biologia',]
 
 function carrega(){
-    for(var item in nome) {
+    for(var item in nome){
         if((item % 2)==0){
-            $('#esq').append('<div class="caixa"><a href="'+nome[item]+'.html" class=txtbox>'+nome[item]+'</a></div>');
+            $('#esq').append('<div class="caixa"><a href="'+nome[item]+'.html"><img src="img/'+nome[item]+'.png"alt="'+nome[item]+'" class="imagem"></a></div>');
         }else{
-            $('#dir').append('<div class="caixa"><a href="'+nome[item]+'.html" class=txtbox>'+nome[item]+'</a></div>');
+            $('#dir').append('<div class="caixa"><a href="'+nome[item]+'.html"><img src="img/'+nome[item]+'.png"alt="'+nome[item]+'" class="imagem"></a></div>');
         }
-        
-    }   
-   
+    }
 }
+        
